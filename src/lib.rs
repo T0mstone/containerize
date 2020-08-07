7b33e7862f928cf3e95ea85139993ab8686322cd
T0mstone <realt0mstone@gmail.com>
2020-08-07 16:41:27 +0200

    added ContainerizedVec changed the API a bit added containerize_chars removed Containerized::collect_single in favor of ContainerizedVec::collapse_single

--- src/lib.rs
+++ src/lib.rs
@@ -24,9 +24,11 @@
 //! (In real code, `C` is called `Contained`, you'd have every single char enclosed in a `Single` and you'd have to create the enum with variants `Squ` and `Par` yourself)
 
 #![warn(missing_docs)]
+use apply::*;
 use bi_result::BiResult;
 use std::iter::{once, FromIterator};
 use std::marker::PhantomData;
+use std::ops::{Deref, DerefMut};
 
 /// The core data type. It represents grouped pieces of a stream.
 ///
@@ -38,7 +40,40 @@ pub enum Containerized<C, T> {
     /// A single item
     Single(T),
     /// A contained block of items
-    Contained(C, Vec<Self>),
+    Contained(C, ContainerizedVec<C, T>),
+}
+
+/// A wrapper type for `Vec<Containerized<C, T>>` with some wrapper methods to shorten code
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct ContainerizedVec<C, T>(pub Vec<Containerized<C, T>>);
+
+impl<C, T> Deref for ContainerizedVec<C, T> {
+    type Target = Vec<Containerized<C, T>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<C, T> DerefMut for ContainerizedVec<C, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<C, T> IntoIterator for ContainerizedVec<C, T> {
+    type Item = Containerized<C, T>;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+impl<C, T> FromIterator<Containerized<C, T>> for ContainerizedVec<C, T> {
+    fn from_iter<I: IntoIterator<Item = Containerized<C, T>>>(iter: I) -> Self {
+        Self(Vec::from_iter(iter))
+    }
 }
 
 /// The side of a delimeter, i.e. left or right
@@ -97,10 +132,14 @@ pub struct UnmatchedDelimeter<C> {
 ///
 /// Note that in `abc[(def])`, both `[` and `]` would be counted as unmatched delimeters
 #[allow(clippy::type_complexity)]
-pub fn containerize<C: PartialEq, I: IntoIterator>(
+pub fn containerize<
+    C: PartialEq,
+    I: IntoIterator,
+    F: FnMut(&I::Item) -> Option<(DelimeterSide, C)>,
+>(
     iter: I,
-    mut detect_delimeter: impl FnMut(&I::Item) -> Option<(DelimeterSide, C)>,
-) -> BiResult<Vec<Containerized<C, Vec<I::Item>>>, Vec<UnmatchedDelimeter<C>>> {
+    mut detect_delimeter: F,
+) -> BiResult<ContainerizedVec<C, Vec<I::Item>>, Vec<UnmatchedDelimeter<C>>> {
     let mut base: Vec<Containerized<C, Vec<I::Item>>> = vec![];
     let mut stack = vec![];
     let mut unmatched = vec![];
@@ -115,7 +154,7 @@ pub fn containerize<C: PartialEq, I: IntoIterator>(
                             .last_mut()
                             .map(|(_, _, v)| v)
                             .unwrap_or(&mut base)
-                            .push(Containerized::Contained(c, last));
+                            .push(Containerized::Contained(c, ContainerizedVec(last)));
                     }
                     _ => {
                         // push an error and ignore it
@@ -166,7 +205,19 @@ pub fn containerize<C: PartialEq, I: IntoIterator>(
         }
         // base.extend(extra);
     }
-    BiResult(base, unmatched)
+    BiResult(ContainerizedVec(base), unmatched)
+}
+
+/// A shortcut that automatically converts a string to chars before containerizing and converts it back to a `String` after containerizing
+pub fn containerize_chars<C: PartialEq, F: FnMut(char) -> Option<(DelimeterSide, C)>>(
+    s: &str,
+    mut detect_delimeter: F,
+) -> BiResult<ContainerizedVec<C, String>, Vec<UnmatchedDelimeter<C>>> {
+    containerize(s.chars(), |&c| detect_delimeter(c)).map(|v| {
+        v.into_iter()
+            .map(|c| c.map(|v| v.into_iter().collect()))
+            .collect()
+    })
 }
 
 impl<C, I: IntoIterator<Item = T>, T> Containerized<C, I> {
@@ -186,28 +237,39 @@ impl<C, I: IntoIterator<Item = T>, T> Containerized<C, I> {
     }
 }
 
-impl<C, T> Containerized<C, T> {
-    /// Convert from an `IntoIterator` of a `Containerized` (e.g. `vec![Single(t1), Single(t2), Contained(...), ...]`)
-    /// to a `Vec` of `Containerized`s of `Vec`s (e.g. `vec![Single(vec![t1, t2]), Contained(...), ...]`)
-    pub fn collect_single<I: IntoIterator<Item = Self>>(v: I) -> Vec<Containerized<C, Vec<T>>> {
-        v.into_iter().fold(vec![], |mut res, e: Self| {
-            match e {
-                Containerized::Single(t) => match res.last_mut() {
-                    Some(Containerized::Single(v)) => v.push(t),
-                    _ => res.push(Containerized::Single(vec![t])),
+impl<C, T> ContainerizedVec<C, T> {
+    /// Convert from a `ContainerizedVec` (e.g. `ContainerizedVec(vec![Single(t1), Single(t2), Contained(...), ...])`)
+    /// to a `ContainerizedVec` of `Vec`s (e.g. `ContainerizedVec(vec![Single(vec![t1, t2]), Contained(...), ...])`)
+    pub fn collapse_single(self) -> ContainerizedVec<C, Vec<T>> {
+        self.into_iter()
+            .fold(
+                vec![],
+                |mut res: Vec<Containerized<C, Vec<T>>>, e: Containerized<C, T>| {
+                    match e {
+                        Containerized::Single(t) => match res.last_mut() {
+                            Some(Containerized::Single(v)) => v.push(t),
+                            _ => res.push(Containerized::Single(vec![t])),
+                        },
+                        Containerized::Contained(c, v) => {
+                            res.push(Containerized::Contained(c, v.collapse_single()))
+                        }
+                    }
+                    res
                 },
-                Containerized::Contained(c, v) => {
-                    res.push(Containerized::Contained(c, Self::collect_single(v)))
-                }
-            }
-            res
-        })
+            )
+            .apply(ContainerizedVec)
     }
 }
 
 pub mod visit;
 
 impl<C, T> Containerized<C, T> {
+    /// A small helper to avoid having to wrap the second argument of `Contained` in a `ContainerizedVec` manually
+    #[inline]
+    pub fn contained(c: C, v: Vec<Self>) -> Self {
+        Self::Contained(c, ContainerizedVec(v))
+    }
+
     /// Returns the container kind if `self` is `Contained`, `None` otherwise
     #[inline]
     pub fn container_kind(&self) -> Option<&C> {
@@ -260,18 +322,17 @@ impl<C, T> Containerized<C, T> {
     /// Maps the value inside a `Single` using the given function, which can return more than one value
     #[inline]
     pub fn multi_map<
-        U,
-        I: IntoIterator<Item = U>,
+        I: IntoIterator,
         F: FnMut(T) -> I,
-        J: FromIterator<Containerized<C, U>>,
+        J: FromIterator<Containerized<C, I::Item>>,
     >(
         self,
         f: F,
     ) -> J {
-        struct Inner<T, U, I: IntoIterator<Item = U>, F: FnMut(T) -> I>(F, PhantomData<(T, U, I)>);
+        struct Inner<T, I: IntoIterator, F: FnMut(T) -> I>(F, PhantomData<(T, I)>);
 
-        impl<T, U, I: IntoIterator<Item = U>, F: FnMut(T) -> I> Inner<T, U, I, F> {
-            pub fn f<C, J: FromIterator<Containerized<C, U>>>(
+        impl<T, I: IntoIterator, F: FnMut(T) -> I> Inner<T, I, F> {
+            pub fn f<C, J: FromIterator<Containerized<C, I::Item>>>(
                 &mut self,
                 c: Containerized<C, T>,
             ) -> J {
@@ -336,10 +397,180 @@ impl<C, T> Containerized<C, T> {
     }
 }
 
+impl<C, T> ContainerizedVec<C, T> {
+    /// A wrapper for [`Containerized::map`](struct.Containerized.html#method.map)
+    #[inline]
+    pub fn map<U, F: FnMut(T) -> U>(self, mut f: F) -> ContainerizedVec<C, U> {
+        self.0.into_iter().map(|c| c.map(&mut f)).collect()
+    }
+
+    /// A wrapper for [`Containerized::multi_map`](struct.Containerized.html#method.multi_map)
+    #[inline]
+    pub fn multi_map<I: IntoIterator, F: FnMut(T) -> I>(
+        self,
+        mut f: F,
+    ) -> ContainerizedVec<C, I::Item> {
+        self.0
+            .into_iter()
+            .flat_map(|c| c.multi_map::<I, _, Vec<_>>(&mut f))
+            .collect()
+    }
+
+    /// A wrapper for [`Containerized::map_kind`](struct.Containerized.html#method.map_kind)
+    #[inline]
+    pub fn map_kind<K, F: FnMut(C) -> K>(self, mut f: F) -> ContainerizedVec<K, T> {
+        self.0.into_iter().map(|c| c.map_kind(&mut f)).collect()
+    }
+
+    /// A wrapper for [`Containerized::uncontainerize`](struct.Containerized.html#method.uncontainerize)
+    #[inline]
+    pub fn uncontainerize<F: FnMut(C) -> (T, T)>(self, mut make_delim: F) -> Vec<T> {
+        self.0
+            .into_iter()
+            .flat_map(|c| c.uncontainerize(&mut make_delim))
+            .collect()
+    }
+}
+
+impl<C, I: IntoIterator> ContainerizedVec<C, I> {
+    /// Split the top-most layer (contents of `Contained` are left untouched) according to `is_sep`,
+    /// which determines, whether an item is a separator
+    pub fn split_top<F: FnMut(&I::Item) -> bool>(
+        self,
+        mut is_sep: F,
+        fuse_repeated: bool,
+    ) -> Vec<ContainerizedVec<C, Vec<I::Item>>> {
+        self.0
+            .into_iter()
+            .fold(vec![ContainerizedVec(vec![])], |mut acc, c| {
+                match c {
+                    Containerized::Single(v) => {
+                        let mut first = true;
+                        for t in v {
+                            if is_sep(&t) {
+                                if !(fuse_repeated && acc.last_mut().unwrap().is_empty()) {
+                                    acc.push(ContainerizedVec(vec![]))
+                                }
+                            } else {
+                                // we preserve the boundaries between `Single` items, we only create new ones
+                                if first {
+                                    acc.last_mut().unwrap().push(Containerized::Single(vec![t]));
+                                } else {
+                                    match acc.last_mut().unwrap().last_mut() {
+                                        Some(Containerized::Single(v)) => v.push(t),
+                                        // this loop always ends with a `Single` as the last element
+                                        // and since it already ran at least once, that is now necessarily the case
+                                        _ => unreachable!(),
+                                    }
+                                }
+                            }
+                            if first {
+                                first = false;
+                            }
+                        }
+                    }
+                    Containerized::Contained(..) => acc
+                        .last_mut()
+                        .unwrap()
+                        .push(c.map(|i| i.into_iter().collect())),
+                }
+                acc
+            })
+    }
+}
+
+impl<C> ContainerizedVec<C, String> {
+    /// A wrapper for [`split_top(|c: char| c.is_whitespace(), true)`](#method.split_top)
+    pub fn split_top_whitespace(self) -> Vec<Self> {
+        self.map(|s| s.chars().collect::<Vec<_>>())
+            .split_top(|u| u.is_whitespace(), true)
+            .into_iter()
+            .map(|v| v.map(|v| v.into_iter().collect::<String>()))
+            .collect()
+    }
+}
+
+impl<C> ContainerizedVec<C, Vec<u8>> {
+    /// A wrapper for [`split_top(|c: char| c.is_ascii_whitespace(), true)`](#method.split_top)
+    pub fn split_top_whitespace(self) -> Vec<Self> {
+        self.split_top(|u| u.is_ascii_whitespace(), true)
+    }
+}
+
+/// A macro to simplify creating a `Containerized` by hand
+///
+/// # Syntax
+/// 1.
+/// ```
+/// # use containerize::{Containerized, containerized};
+/// let c = containerized!(3 => [4, 5]);
+/// assert_eq!(c, Containerized::contained(3, vec![Containerized::Single(4), Containerized::Single(5)]));
+/// ```
+/// 2.
+/// ```
+/// # use containerize::{Containerized, containerized};
+/// let c = containerized!("single");
+/// assert_eq!(c, Containerized::<(), &str>::Single("single"));
+/// ```
+#[macro_export]
+macro_rules! containerized {
+    ($c:expr => [$($e:expr $(=> [$($t:tt)*])?),*$(,)?]) => {
+        $crate::Containerized::contained($c, vec![$($crate::containerized!($e $(=> [$($t)*])?)),*])
+    };
+    ($e:expr) => {
+        $crate::Containerized::Single($e)
+    };
+}
+
+/// A macro to simplify creating a `ContainerizedVec`, building upon [`containerized!`](macro.containerized.html)
+///
+/// # Syntax
+/// ```
+/// # use containerize::{Containerized, ContainerizedVec, containerized_vec};
+/// let c = containerized_vec![1, 2 => [3, 4 => [5]], 6];
+/// assert_eq!(c, ContainerizedVec(vec![
+///     Containerized::Single(1),
+///     Containerized::contained(2, vec![
+///         Containerized::Single(3), Containerized::contained(4, vec![Containerized::Single(5)])
+///     ]),
+///     Containerized::Single(6)
+/// ]));
+/// ```
+#[macro_export]
+macro_rules! containerized_vec {
+    ($($e:expr $(=> [$($t:tt)*])?),*$(,)?) => {
+        $crate::ContainerizedVec(vec![$($crate::containerized!($e $(=> [$($t)*])?)),*])
+    };
+}
+
 #[cfg(test)]
 mod tests {
     use crate::visit::*;
     use crate::*;
+
+    #[test]
+    fn r#macro() {
+        let v = containerized_vec![() => [1u8, 2, 3], 4, () => [() => [() => [5]]]];
+        let ctrl = vec![
+            Containerized::contained(
+                (),
+                vec![
+                    Containerized::Single(1u8),
+                    Containerized::Single(2),
+                    Containerized::Single(3),
+                ],
+            ),
+            Containerized::Single(4),
+            Containerized::contained(
+                (),
+                vec![Containerized::contained(
+                    (),
+                    vec![Containerized::contained((), vec![Containerized::Single(5)])],
+                )],
+            ),
+        ];
+        assert_eq!(v.0, ctrl);
+    }
 
     #[test]
     fn containerized() {
@@ -354,9 +585,9 @@ mod tests {
             }
         });
         assert_eq!(
-            c,
+            c.0,
             vec![
-                Containerized::Contained((), vec![Containerized::Single(vec![2])]),
+                Containerized::contained((), vec![Containerized::Single(vec![2])]),
                 Containerized::Single(vec![2, 2])
             ]
         );
@@ -389,22 +620,12 @@ mod tests {
                 Containerized::Single(4)
             ]
         );
-        assert_eq!(Containerized::collect_single(cs), vec![c]);
+        assert_eq!(Containerized::collapse_single(cs), vec![c]);
     }
 
     #[test]
     fn visit() {
-        let mut c = Containerized::<(), _>::Contained(
-            (),
-            vec![
-                Containerized::Single(3),
-                Containerized::Contained(
-                    (),
-                    vec![Containerized::Single(4), Containerized::Single(5)],
-                ),
-                Containerized::Single(6),
-            ],
-        );
+        let mut c = containerized![() => [3, () => [4, 5], 6]];
         let mut sum = 0;
         c.visit(|x| {
             if let Containerized::Single(n) = *x {
@@ -421,22 +642,7 @@ mod tests {
                 v.push(Containerized::Single(1));
             }
         });
-        let ctrl = Containerized::<(), _>::Contained(
-            (),
-            vec![
-                Containerized::Single(4),
-                Containerized::Contained(
-                    (),
-                    vec![
-                        Containerized::Single(5),
-                        Containerized::Single(6),
-                        Containerized::Single(2),
-                    ],
-                ),
-                Containerized::Single(7),
-                Containerized::Single(2),
-            ],
-        );
+        let ctrl = containerized!(() => [4, () => [5, 6, 2], 7, 2]);
         assert_eq!(c, ctrl);
         c2.visit_with_config(
             |x| match x {
@@ -452,22 +658,7 @@ mod tests {
                 ..Default::default()
             },
         );
-        let ctrl = Containerized::<(), _>::Contained(
-            (),
-            vec![
-                Containerized::Single(4),
-                Containerized::Contained(
-                    (),
-                    vec![
-                        Containerized::Single(5),
-                        Containerized::Single(6),
-                        Containerized::Single(1),
-                    ],
-                ),
-                Containerized::Single(7),
-                Containerized::Single(1),
-            ],
-        );
+        let ctrl = containerized!(() => [4, () => [5, 6, 1], 7, 1]);
         assert_eq!(c2, ctrl);
     }
 
@@ -475,20 +666,20 @@ mod tests {
     fn map() {
         let c = Containerized::<(), u8>::Single(3);
         assert_eq!(c.map(|x| x + 1), Containerized::Single(4));
-        let c = Containerized::<(), u8>::Contained(
+        let c = Containerized::<(), u8>::contained(
             (),
             vec![Containerized::Single(2), Containerized::Single(3)],
         );
-        let ctrl = Containerized::<(), u8>::Contained(
+        let ctrl = Containerized::<(), u8>::contained(
             (),
             vec![Containerized::Single(3), Containerized::Single(4)],
         );
         assert_eq!(c.map(|x| x + 1), ctrl);
-        let c = Containerized::<u8, u8>::Contained(
+        let c = Containerized::<u8, u8>::contained(
             1,
             vec![Containerized::Single(2), Containerized::Single(3)],
         );
-        let ctrl = Containerized::<u8, u8>::Contained(
+        let ctrl = Containerized::<u8, u8>::contained(
             2,
             vec![Containerized::Single(2), Containerized::Single(3)],
         );
@@ -497,23 +688,11 @@ mod tests {
 
     #[test]
     fn join() {
-        let c = Containerized::<(), _>::Contained(
-            (),
-            vec![
-                Containerized::Contained(
-                    (),
-                    vec![
-                        Containerized::Contained((), vec![]),
-                        Containerized::Contained((), vec![Containerized::Single(3)]),
-                    ],
-                ),
-                Containerized::Single(3),
-                Containerized::Single(4),
-            ],
-        );
+        let c = containerized!(() => [() => [() => [], () => [b'3']], b'3', b'4']);
         assert_eq!(
             c.uncontainerize(|_| (b'(', b')')),
-            vec![b'(', b'(', b'(', b')', b'(', 3, b')', b')', 3, 4, b')']
+            b"((()(3))34)".to_vec(),
+            // vec![b'(', b'(', b'(', b')', b'(', 3, b')', b')', 3, 4, b')']
         );
     }
 }
